55b17e879e70b0f3c72d771624bc8cf0994ee804
Robin BROQUERIE <robin.broquerie@gmail.com>
2019-07-09 11:56:23 +0200

    tableau

--- 02.tableau.js
+++ 02.tableau.js
@@ -1,1 +1,27 @@
+var lg = console.log;
 var villes = ['nantes','paris','saint-nazaire','angers','le mans'];
+
+villes.forEach(function (valeur) {
+    lg(valeur);
+});
+
+var lettreADansToutesLesVilles = villes.every(function (value) {
+    return value.includes('a');
+});
+
+lg('lettreADansToutesLesVilles = '+lettreADansToutesLesVilles);
+
+var villesSansTiretSansEspace = villes.filter(function (value) {
+    return (!value.includes('-') && !value.includes(' '))
+});
+
+lg(villesSansTiretSansEspace);
+
+var villesMajusculeSeTerminantParS = villes.filter(function (value) {
+    return value.endsWith('s');
+}).map(function (value) {
+    return value.toUpperCase();
+});
+
+lg(villesMajusculeSeTerminantParS);
+
